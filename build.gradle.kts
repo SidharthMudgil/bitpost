509724a02e0f0c3d5d5c36002f6ca4d62947f192
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-11-28 15:07:52 +0530

    bump ksp version to 1.9.21-1.0.15

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.7.3" apply false
 }
